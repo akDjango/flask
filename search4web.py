505faadf225a1c6cb6866b1878659de197a59484
audie <kayela010@gmail.com>
2020-05-30 15:18:15 +0200

    exception handling and a bit of threading

--- search4web.py
+++ search4web.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, escape, session
+from flask import copy_current_request_context
 from vsearch import search_for_letters
-from checker import check_logged_in
 
-from DBcm import UseDatabase
+from checker import check_logged_in
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
+
+from threading import Thread
+from time import sleep
 
 app = Flask(__name__)
 
@@ -11,44 +15,67 @@ app.config['dbconfig'] =  { 'host': '127.0.0.1',
                  'password': 'vsearchpasswd',
                  'database': 'vsearchlogDB',}
 
-def log_request(req: 'flask_request', res: str) -> None:
-    """Log details of the web request and the results."""
-
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """insert into log
-              (phrase, letters, ip, browser_string, results)
-              values
-              (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form['phrase'],
-                          req.form['letters'],
-                          req.remote_addr,
-                          req.user_agent.browser,
-                          res,))
-
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
+
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        """ Log the details of the web request and result """
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """ insert into log
+                        (phrase, letters, ip, browser_string, results)
+                        values
+                        (%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL,(req.form['phrase'],
+                                req.form['letters'],
+                                req.remote_addr,
+                                req.user_agent.browser,
+                                res, ))
+
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search_for_letters(phrase,letters))
-    log_request(request, results)
-    return render_template('results.html', the_phrase=phrase, the_letters=letters, the_title=title, the_results=results,)
+    try :
+        t = Thread(target = log_request, args = (request, results))
+        t.start()
+    except Exception as err:
+        print('***** Logging failed with this error:',  str(err))
+    return render_template('results.html',
+                            the_phrase=phrase,
+                            the_letters=letters,
+                            the_title=title,
+                            the_results=results,)
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+    return render_template('entry.html',
+            the_title='Welcome to Ak-search4letters on the web!')
 
 @app.route('/viewlog')
 @check_logged_in
 def view_the_log() -> 'html':
     """Display the contents of the log file as an HTML table. """
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """select phrase, letters, ip, browser_string, results from log"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
-    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html', the_title = 'View Log', the_row_titles = titles, the_data = contents,)
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """select phrase, letters, ip, browser_string, results from log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+        return render_template('viewlog.html',
+                the_title = 'View Log',
+                the_row_titles = titles,
+                the_data = contents,)
+    except ConnectionError as err:
+        print('Is your database switched on? Error:', str(err))
+    except CredentialsError as err:
+        print('User-id/password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your querry correct? Error:', str(err))
+    except Exception as err:
+        print('Something went wrong:', str(err))
+    return 'Error'
 
 @app.route('/login')
 def do_login() -> str:
@@ -63,4 +90,4 @@ def do_logout() -> str:
 app.secret_key = 'ItoldYouYouWillNeverGuessThisMySecretKey'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
